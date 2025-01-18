e563e73a86aadcb78c6fa5eff3205afa0477a362
David Jonatan <dev2@pastpost.com>
2025-01-17 23:10:12 -0600

    modify route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('testamentos')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get(':path?')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get()
+  getAll(): string {
+    return 'Hola desde /testamentos';
+  }
+
+  @Get(':path')
+  getHello(@Param('path') path: string): string {
+    return this.appService.getHello() + `, path param = ${path}`;
   }
 }
