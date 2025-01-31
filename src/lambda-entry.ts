0e2224d94fc2456e8a28f26dfe6dcf25dc988117
Carlos Gutierrez <carlos.gtz.cg@gmail.com>
2025-01-30 19:33:50 -0600

    fix cors error

--- src/lambda-entry.ts
+++ src/lambda-entry.ts
@@ -1,32 +1,64 @@
-import { configure as serverlessExpress } from '@codegenie/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 let cachedServer;
 
-export const handler = async (event, context) => {
+async function bootstrap() {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
-    cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type, Authorization',
     });
+    await nestApp.init();
+    cachedServer = serverlessExpress({ app: expressApp });
   }
-  console.log('Handler called');
-  console.log('Original event:', event);
+  return cachedServer;
+}
 
-  if (event.Records) {
-    console.log('Request comes from SQS queue');
-    try {
-      event = JSON.parse(event.Records[0].body);
-    } catch (error) {
-      console.log('Error parsing SQS body');
-      console.log('Error:', error);
-      event = event.Records[0];
-    }
-  } else {
-    console.log('Request comes from API Gateway');
-  }
-  console.log('Processed event:', event);
-  return cachedServer(event, context);
+export const handler = async (event, context) => {
+  const server = await bootstrap();
+  return server(event, context);
 };
+
+
+// import { configure as serverlessExpress } from '@codegenie/serverless-express';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// let cachedServer;
+
+// export const handler = async (event, context) => {
+//   if (!cachedServer) {
+//     const nestApp = await NestFactory.create(AppModule);
+//     await nestApp.init();
+//     cachedServer = serverlessExpress({
+//       app: nestApp.getHttpAdapter().getInstance(),
+//     });
+//   }
+//   console.log('Handler called');
+//   console.log('Original event:', event);
+
+//   if (event.Records) {
+//     console.log('Request comes from SQS queue');
+//     try {
+//       event = JSON.parse(event.Records[0].body);
+//     } catch (error) {
+//       console.log('Error parsing SQS body');
+//       console.log('Error:', error);
+//       event = event.Records[0];
+//     }
+//   } else {
+//     console.log('Request comes from API Gateway');
+//   }
+//   console.log('Processed event:', event);
+//   return cachedServer(event, context);
+// };
