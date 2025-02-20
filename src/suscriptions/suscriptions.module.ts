0da6e76eb60df0f01af46191a04bec220c8b8587
jonatan <jonatanzch@gmail.com>
2025-02-20 15:58:08 -0600

    #3124 Add PrismaProvider and ConfigService to SuscriptionsModule providers

--- src/suscriptions/suscriptions.module.ts
+++ src/suscriptions/suscriptions.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SuscriptionsService } from './suscriptions.service';
 import { SuscriptionsController } from './suscriptions.controller';
+import { PrismaProvider } from 'src/providers';
+import { ConfigService } from '../config';
 
 @Module({
   controllers: [SuscriptionsController],
-  providers: [SuscriptionsService],
+  providers: [SuscriptionsService, PrismaProvider, ConfigService],
 })
 export class SuscriptionsModule {}
