e07270d9851cf1ccd4789057d155dd3c49c22439
jonatan <jonatanzch@gmail.com>
2025-03-10 03:16:52 -0600

    Refactor PDF streaming logic to remove response parameter and return Base64 encoded PDF

--- src/testaments/testaments.service.ts
+++ src/testaments/testaments.service.ts
@@ -10,8 +10,8 @@ import { GeneralResponseDto, PaginationDto } from '../common';
 import { processException } from '../common/utils/exception.helper';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { UpdateTestamentStatusDto } from './dto/update-testament-tatus.dto';
-import { Response } from 'express';
-import { Readable } from 'stream';
+// import { Response } from 'express';
+// import { Readable } from 'stream';
 @Injectable()
 export class TestamentsService {
   private prisma: any = null;
@@ -587,7 +587,7 @@ export class TestamentsService {
     }
   }
 
-  async streamTestamentPdf(testamentId: string, res: Response) {
+  async streamTestamentPdf(testamentId: string) {
     const response = new GeneralResponseDto();
     try {
       this.prisma = await this.prismaProvider.getPrismaClient();
@@ -672,14 +672,19 @@ export class TestamentsService {
           );
         }
 
-        res.set({
-          'Content-Type': 'application/pdf',
-          'Content-Disposition': `inline; filename="${testamentId}.pdf"`,
-        });
-        const nodeStream = Readable.from(response.Body.transformToWebStream());
-
-        console.log('[streamTestamentPdf] Streaming PDF to API Gateway...');
-        nodeStream.pipe(res);
+        const buffer = Buffer.from(await response.Body.transformToByteArray());
+        console.log(
+          '[streamTestamentPdf] Returning Base64 PDF to API Gateway...',
+        );
+        return {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': `inline; filename="${testamentId}.pdf"`,
+          },
+          body: buffer.toString('base64'),
+          isBase64Encoded: true,
+        };
       } catch (error) {
         console.log('Error reading from S3:', error);
         response.code = 500;
